b2ff83e4f035a83e45974c3b6b154e2e98619c4c
JosephSneha <73291783+JosephSneha@users.noreply.github.com>
2020-12-05 16:15:57 +0530

    updated

--- proj/src/app/app.component.ts
+++ proj/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {​ Component, OnInit }​ from '@angular/core';
+@Component({​
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+}​)
+export class AppComponent implements OnInit {​
+    title = 'Street Styles';
+    public isSubmitted = false;
+    constructor() {​}​
+    ngOnInit() {​
+    }​
+    submit() {​
+        this.isSubmitted = true;
+    }​
+}​
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'proj';
-}
